b2b7f01a0b78105588c961a8a7bc1d1ea2a88382
Tatu <tatupetteri2312@gmail.com>
2024-08-23 13:45:42 +0300

    Small changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todolistreact/',
   plugins: [react()],
+  build: {
+    outDir: 'build',  
+  },
   test: {
     globals: true,
     environment: 'jsdom',
